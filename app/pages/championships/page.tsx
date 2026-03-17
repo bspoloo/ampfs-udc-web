3d95763a51e1c778295a00c379bd2d51ca371084
bspoloo <animetx098@gmail.com>
2026-03-17 02:54:25 -0400

    test to deploy 4

--- app/pages/championships/page.tsx
+++ app/pages/championships/page.tsx
@@ -3,6 +3,6 @@
 export default function Championships(){
     return <div>
         {process.env.BACKEND_API_URL}
-        <h1> this is the page of Championships</h1>
+        <h1> this is the page of Championships xd</h1>
     </div>
 }
