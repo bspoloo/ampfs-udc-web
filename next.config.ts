21269cb3c4d178edf6ee87dc2eae6006cf112be6
bspoloo <animetx098@gmail.com>
2026-03-17 02:36:26 -0400

    test to deploy 1

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,38 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  distDir: 'build',
+  output: 'standalone',
+  env: {
+    BACKEND_API_URL: process.env.BACKEND_API_URL
+  }, 
+  turbopack: {},
+  webpack(config) {
+    config.module.push({
+      test: /\.svg$/,
+      use: [{ loader: "@svgr/webpack", options: { icon: true } }]
+    })
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/',
+        destination: '/',
+      },
+      {
+        source: '/ampfs-udc/championships',
+        destination: '/pages/championships',
+      },
+
+
+      //always add in the final
+      {
+        source: '/:path*',
+        destination: '/'
+      }
+    ]
+  }
 };
 
 export default nextConfig;
