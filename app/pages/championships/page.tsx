1f2a995de2094dfbc626a44c18d0dbc914b307e2
DgteMendez <pilotomendez123@gmail.com>
2026-04-02 17:04:15 -0400

    mendez/ updating changes of frontend in championship

--- app/pages/championships/page.tsx
+++ app/pages/championships/page.tsx
@@ -1,12 +1,18 @@
+import ChampionshipsHeader from "@/app/components/championships/championships-header";
+import ChampionshipsList from "@/app/components/championships/championships-list";
+import Pagination from "@/app/components/championships/pagination";
 import Sidebar from "@/app/components/layout/sidebar/sidebar";
 
 export default function ChampionshipsPage() {
     return (
-        <div className="flex">
+        <div className="flex h-screen bg-(--bg-main)">
             <Sidebar />
-            <div>
-                <h2 className="text-white p-10">Campeonato</h2>
-            </div>
+
+            <main className="flex-1 p-6 overflow-y-auto">
+                <ChampionshipsHeader />
+                <ChampionshipsList />
+                <Pagination />
+            </main>
         </div>
     );
 }
