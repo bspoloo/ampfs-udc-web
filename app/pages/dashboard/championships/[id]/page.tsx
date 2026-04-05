1976569768a6972906ed2a54f95362584cb9b719
DgteMendez <pilotomendez123@gmail.com>
2026-04-05 16:38:41 -0400

    mendez/ending with the changes to the championship details

--- app/pages/dashboard/championships/[id]/page.tsx
+++ app/pages/dashboard/championships/[id]/page.tsx
@@ -1,7 +1,24 @@
+import Categories from "@/app/components/championships/categories";
+
 export default function ChampionshipDetail() {
     return (
-        <div className="p-6 text-white">
-            <h1>Detalle del campeonato 1</h1>
+        <div className="text-white -m-6">
+            <div className="relative w-full h-40 sm:h-52 md:h-64 overflow-hidden">
+                <img
+                    src="/championship_detail.webp"
+                    className="w-full h-full object-cover"
+                />
+                <div className="absolute inset-0 bg-black/60" />
+                <div className="absolute top-4 left-0 w-full px-4 sm:px-6 flex flex-col sm:flex-row sm:items-center sm:justify-between gap-3">
+                    <h2 className="text-xl font-semibold text-(--text-sidebar)">Campeonato clausura 2/2026</h2>
+                    <select className="bg-(--bg-sidebar) border border-(--border-dark) text-(--text-sidebar) px-4 py-2 rounded-md text-sm outline-none cursor-pointer">
+                        <option value="activo">Activo</option>
+                        <option value="proceso">En proceso</option>
+                        <option value="finalizado">Finalizado</option>
+                    </select>
+                </div>
+            </div>
+            <Categories />
         </div>
     );
 }
